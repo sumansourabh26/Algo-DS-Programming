bdf6437af53e448adb4d45c06de830b0112df92d
Suman Sourabh <sumansourabh26@gmail.com>
2024-11-03 09:03:04 +0530

    Time: 51 ms (80.42%), Space: 16.7 MB (94.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -3,22 +3,22 @@ class Solution:
         l1 = len(word1)+1
         l2 = len(word2)+1
 
-        dp = [ [0]*l2 for i in range(l1) ]
+        # dp = [ [0]*l2 for i in range(l1) ]
 
-        dp[0] = [i for i in range(l2)]
-
-        for i in range(l1):
-            dp[i][0] = i
+        dp = [i for i in range(l2)]
         
         for i in range (1, l1):
-            
+
+            new_dp = [None] * l2
+            new_dp[0] = i
             for j in range (1, l2):
                 if(word1[i-1] == word2[j-1]) : 
-                    dp[i][j] = dp[i-1][j-1] 
-                    # min(dp[i-1][j-1], dp[i-1][j]+1, dp[i][j-1]+1)
+                    new_dp[j] = dp[j-1] 
                 else : 
-                    dp[i][j] = 1+ min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])
+                    new_dp[j] = 1+ min(dp[j-1], dp[j], new_dp[j-1])
+            dp = new_dp
+            del new_dp
         
-        return dp[l1-1][l2-1]
+        return dp[l2-1]
 
         
