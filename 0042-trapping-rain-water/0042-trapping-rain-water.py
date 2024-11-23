a73b7436cbb9163b3bcc8bb2e1ea83b0cb7011ff
Suman Sourabh <sumansourabh26@gmail.com>
2024-11-23 11:19:02 +0530

    Time: 31 ms (24.67%), Space: 18.3 MB (71.02%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -7,18 +7,23 @@ class Solution:
          i 0 
         '''
         length = len(height)
-        left_max = [None]*length
+        # left_max = [None]*length
         right_max = [None]*length
         
-        left_max[0] = 0
+        # left_max[0] = 0
         right_max[length-1] = 0
 
         for i in range(1, length):
-            left_max[i] = int(max(left_max[i-1], height[i-1]))
+            # left_max[i] = int(max(left_max[i-1], height[i-1]))
             right_max[length-1 - i] = (max(right_max[length- i], height[length- i]))
         
         water = 0
+        left_max = height[0]
         for i in range(1, length):
-            water +=  max(min(left_max[i], right_max[i]) - height[i], 0)
+            # print(left_max)
+            water +=  max(min(left_max, right_max[i]) - height[i], 0)
+            left_max = max(left_max, height[i])
+
+
         
         return water
