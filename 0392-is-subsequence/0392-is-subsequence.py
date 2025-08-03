a57454d8a9e45cda081c08fc3208b23f10503bc5
Suman Sourabh <sumansourabh26@gmail.com>
2025-08-03 17:51:14 +0530

    Time: 4 ms (9.35%), Space: 19.2 MB (5.12%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,17 +1,14 @@
+def helper(s, s_i, t, t_i) : 
+    if(s_i == len(s))  : 
+        return True
+    if( t_i == len(t)):
+        return False
+    if(s[s_i] == t[t_i]): 
+        return helper(s, s_i+1, t, t_i+1)
+    return helper(s, s_i, t, t_i+1)
+
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        return helper(s,0,t,0)
 
-        
-        i = 0
-        j = 0
-        
-        while (i < len(s) and j < len(t)):
-            
-            if(s[i] == t[j]) :
-                i+=1
-            j+=1
-                
-        else:
-            if(i == len(s)) : return True
-            if(j == len(t)) : return False
         
