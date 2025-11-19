84f359e06d7d8764a9b022f85ce040674d463b26
Suman Sourabh <sumansourabh26@gmail.com>
2025-11-19 22:32:11 +0530

    Time: 18 ms (72.39%), Space: 23.5 MB (19.86%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        min_index = None
+        min_value = float('inf')
+
+        current_fuel = 0
+
+        for i in range(len(gas)):
+            current_fuel += gas[i] - cost[i]
+            
+            if(current_fuel < min_value) : 
+                min_index = i
+                min_value = current_fuel
+        
+        if current_fuel < 0 : return -1
+
+        return (min_index + 1)%len(gas)
